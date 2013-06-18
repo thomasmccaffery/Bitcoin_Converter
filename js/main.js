d3ee897ba6dac088217d49c84109a23b7ded9473
Thomas McCaffery <tom@thomasmccaffery.com>
2013-06-17 23:52:22 -0400

    Updated function to allow for rounding and resize

--- js/main.js
+++ js/main.js
@@ -7,16 +7,27 @@ function XChange(val,type)
 		//Converts Dollar Amount to Bitcoins
 	    if(type==1)
 	     {
-			var Rate = val/json.data.last.value;
+			var Rate = addCommas((val/json.data.last.value).toFixed(5));
 			document.getElementById("BTCval").value=Rate;
 			document.getElementById('loading').style.display = "none";
+			document.getElementById("USDval").style.width=(val.toString().length*17)+"px";
+			document.getElementById("BTCval").style.width=(Rate.toString().length*17)+"px";
 	     } 
 	     //Converts Bitcoins to Dollar Amount
 	    else if(type==2)
 	     {
-			var Rate = val*json.data.last.value;
+			var Rate = addCommas((val*json.data.last.value).toFixed(5));
 			document.getElementById("USDval").value=Rate;
 			document.getElementById('loading').style.display = "none";
+			document.getElementById("BTCval").style.width=(val.toString().length*17)+"px";
+			document.getElementById("USDval").style.width=(Rate.toString().length*17)+"px";
+
 	     }
 	});
+}
+
+function addCommas(x) {
+    var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
 }
